f368b8f6747c74d686fd97f5580fe5ac5e15f197
Danny Xu <dannyxu@Dannys-MBP.ad.edenpr.org>
2022-01-18 16:37:52 -0600

    worked on cnn scraper

--- scrapers/cnn.js
+++ scrapers/cnn.js
@@ -1,1 +1,31 @@
-/*Scraping CNN home page for news stories*/
+const cheerio = require('cheerio');
+const { last } = require('cheerio/lib/api/traversing');
+const request = require('request');
+
+request({
+    
+    method: 'GET',
+    url: 'https://www.cnn.com/'
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+    let $ = cheerio.load(body);
+    let headlines = [];
+    $('div').each(function (i, e) {
+        var post = $(this)[0];
+        console.log(post);
+        headlines[i] = $(this).text();
+    });
+
+    console.log(headlines);
+   
+    //console.log(headlines);
+});
+
+/*   statusText.forEach((word) => {
+    if (word) {
+        word = word.replace(/(\s+)/g, '');
+    }
+});
+
+*/
